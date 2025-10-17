224c3b8b333491f5fe028afaf609c1c138a9840d
LoiselK <lnkleijnen@gmail.com>
2025-10-17 01:06:06 +0000

    Step 2 Complete - Drawing Supported in Sketchpad

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,24 @@ const clearBtn = document.getElementById("clearBtn") as HTMLButtonElement;
 clearBtn.onclick = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+  cursor.active = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  cursor.active = false;
+});
